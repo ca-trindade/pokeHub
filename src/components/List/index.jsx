fb060d11e9f7ecffb3b131c7601c6f8e45119e79
ca-trindade <piratum.core@gmail.com>
2023-11-24 15:02:42 +0000

    merge

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {Container,
   OrderList,
   ListPokemon,
